efe184556ab17b8515fd88a99d494aad9523b152
Sirat Mokha <mokha1671@gmail.com>
2022-06-14 10:54:15 -0500

    C# Test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Sirat Mokha");
+Console.WriteLine("Brookfield Central High School"); 
+Console.WriteLine("None"); 
