9a785c054ed4492d814b6658c2b67c23892d1f62
Holly R <hraetz14@gmail.com>
2022-06-14 10:55:39 -0500

    Adding my info

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Holly Raetz");
+Console.WriteLine("Attending New Berlin West next school year");
+Console.WriteLine("The worst gift I've recieved was a tiny skillet for making cookies.");
