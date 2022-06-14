6ba2d82169d3d71a09c5e83e92ae8173f805788d
Noah Ren <nren2025@gmail.com>
2022-06-14 11:02:07 -0500

    C# Intro
    Introduction to C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, my name is Noah Ren");
+Console.WriteLine("I will be attending Brookfield East High School next year");
+Console.WriteLine("The worst gift I've received is a book about public speaking");
+
